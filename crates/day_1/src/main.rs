2ac96fa0971e4fdd6df54a6a2532ccd26fce12e9
mohiiit <mohitdhattarwal.personal@gmail.com>
2025-12-11 03:59:05 +0530

    feat: more rust like

--- crates/day_1/src/main.rs
+++ crates/day_1/src/main.rs
@@ -8,6 +8,69 @@ use error::{InputPathError, InstructionParseError};
 use types::{Direction, Instruction};
 use utils::{Part, parse_part};
 
+const DIAL_START: u32 = 50;
+
+pub trait Rotater {
+    fn move_left(&self, current_pos: u32, steps: u32) -> (u32, u32);
+    fn move_right(&self, current_pos: u32, steps: u32) -> (u32, u32);
+}
+
+pub struct Part1Way;
+pub struct Part2Way;
+
+impl Rotater for Part1Way {
+    fn move_left(&self, current_pos: u32, steps: u32) -> (u32, u32) {
+        let new_pos = (current_pos + 100 - steps % 100) % 100;
+        (new_pos, (new_pos == 0) as u32)
+    }
+    fn move_right(&self, current_pos: u32, steps: u32) -> (u32, u32) {
+        let new_pos = (current_pos + steps % 100) % 100;
+        (new_pos, (new_pos == 0) as u32)
+    }
+}
+
+impl Rotater for Part2Way {
+    fn move_left(&self, current_pos: u32, steps: u32) -> (u32, u32) {
+        let rem = steps % 100;
+        let mut answer = steps / 100;
+
+        if current_pos != 0 && rem >= current_pos {
+            answer += 1;
+        }
+        ((current_pos + 100 - rem) % 100, answer)
+    }
+    fn move_right(&self, current_pos: u32, steps: u32) -> (u32, u32) {
+        let temp_current = current_pos;
+        let new_pos = (current_pos + steps % 100) % 100;
+        let mut answer = steps / 100;
+        if temp_current > new_pos {
+            answer += 1;
+        }
+        (new_pos, answer)
+    }
+}
+
+pub trait Solver {
+    fn solve(&self, instructions: &[Instruction]) -> u32;
+}
+
+impl<S: Rotater> Solver for S {
+    fn solve(&self, instructions: &[Instruction]) -> u32 {
+        let mut current_dial = DIAL_START;
+        let mut answer = 0;
+
+        for instruction in instructions {
+            let (new_pos, count) = match instruction.direction {
+                Direction::Left => self.move_left(current_dial, instruction.steps),
+                Direction::Right => self.move_right(current_dial, instruction.steps),
+            };
+            current_dial = new_pos;
+            answer += count;
+        }
+        answer
+    }
+}
+
 fn parse_instruction(line: &str) -> Result<Instruction, InstructionParseError> {
     let first_char = line.chars().next();
     let direction = match first_char {
@@ -37,73 +100,16 @@ fn parse_input_path(arg: Option<String>) -> Result<Vec<Instruction>, InputPathEr
     }
 }
 
-fn move_right(current_dial: &mut u32, instruction: u32, answer: &mut u32) {
-    *current_dial += instruction % 100;
-    *current_dial %= 100;
-    *answer += (*current_dial == 0) as u32;
-}
-
-fn move_left(current_dial: &mut u32, instruction: u32, answer: &mut u32) {
-    *current_dial = (*current_dial + 100 - instruction % 100) % 100;
-    *answer += (*current_dial == 0) as u32;
-}
-
-fn move_right_second(current_dial: &mut u32, instruction: u32, answer: &mut u32) {
-    let temp_current = *current_dial;
-    *current_dial += instruction % 100;
-    *current_dial %= 100;
-    *answer += instruction / 100;
-    if temp_current > *current_dial {
-        *answer += 1;
-    }
-}
-
-fn move_left_second(current_dial: &mut u32, instruction: u32, answer: &mut u32) {
-    let steps = instruction % 100;
-    *answer += instruction / 100;
-    if steps >= *current_dial && *current_dial != 0 {
-        *answer += 1;
-    }
-    *current_dial = (*current_dial + 100 - steps) % 100;
-}
-const DIAL_START: u32 = 50;
-
-fn solve_part_one(instructions: &Vec<Instruction>) -> u32 {
-    let mut current_dial = DIAL_START;
-    let mut answer = 0;
-
-    instructions
-        .iter()
-        .for_each(|instruction| match instruction.direction {
-            Direction::Left => move_left(&mut current_dial, instruction.steps, &mut answer),
-            Direction::Right => move_right(&mut current_dial, instruction.steps, &mut answer),
-        });
-    answer
-}
-
-fn solve_part_two(instructions: &Vec<Instruction>) -> u32 {
-    let mut current_dial = DIAL_START;
-    let mut answer = 0;
-
-    instructions
-        .iter()
-        .for_each(|instruction| match instruction.direction {
-            Direction::Left => move_left_second(&mut current_dial, instruction.steps, &mut answer),
-            Direction::Right => {
-                move_right_second(&mut current_dial, instruction.steps, &mut answer)
-            }
-        });
-    answer
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let inputs: Vec<String> = env::args().collect();
     let part = parse_part(inputs.get(1).cloned())?;
     let instructions = parse_input_path(inputs.get(2).cloned())?;
-    let answer = match part {
-        Part::First => solve_part_one(&instructions),
-        Part::Second => solve_part_two(&instructions),
+
+    let solver: Box<dyn Solver> = match part {
+        Part::First => Box::new(Part1Way),
+        Part::Second => Box::new(Part2Way),
     };
+    let answer = solver.solve(&instructions);
     println!("answer is: {:?}", answer);
     Ok(())
 }
