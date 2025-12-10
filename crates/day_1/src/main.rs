ae0a9460b23a0cd8a4e37e6f11b512cc27774901
mohiiit <mohitdhattarwal.personal@gmail.com>
2025-12-11 03:22:47 +0530

    day 1: rust idomatic used

--- crates/day_1/src/main.rs
+++ crates/day_1/src/main.rs
@@ -1,44 +1,54 @@
-use std::fs;
 use std::env;
+use std::fs;
+use thiserror::Error;
 
 enum Part {
     First,
-    Second
+    Second,
 }
-#[derive(Debug)]
+#[derive(Debug, Error)]
 enum PartError {
+    #[error("Invalid Part: {0}")]
     Invalid(String),
-    Missing
-}
-
-#[derive(Debug)]
-enum InputPathError {
+    #[error("Missing Part")]
     Missing,
-    UnableToReadFrom(String),
-    ParseError(InstructionParseError)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
+enum InputPathError {
+    #[error("Missing Input Path")]
+    Missing,
+    #[error("Unable to read input from: {0}")]
+    UnableToReadFrom(String),
+    #[error("Parse Error: {0}")]
+    ParseError(InstructionParseError),
+}
+
+#[derive(Debug, Error)]
 enum InstructionParseError {
+    #[error("Wrong Direction given: {0}")]
     WrongDirection(char),
+    #[error("No Direction given")]
     NoDirection,
-    StepsParsingFailed
+    #[error("Steps parsing failed")]
+    StepsParsingFailed,
 }
 
 enum Direction {
-    Left, Right
+    Left,
+    Right,
 }
 
 struct Instruction {
     direction: Direction,
-    steps: u32
+    steps: u32,
 }
 fn parse_part(arg: Option<String>) -> Result<Part, PartError> {
     match arg.as_deref() {
         Some("1") => Ok(Part::First),
         Some("2") => Ok(Part::Second),
         Some(random) => Err(PartError::Invalid(random.to_string())),
-        None => Err(PartError::Missing)
+        None => Err(PartError::Missing),
     }
 }
 
@@ -48,95 +58,29 @@ fn parse_instruction(line: &str) -> Result<Instruction, InstructionParseError> {
         Some('L') => Direction::Left,
         Some('R') => Direction::Right,
         Some(random) => return Err(InstructionParseError::WrongDirection(random)),
-        None => return Err(InstructionParseError::NoDirection)
+        None => return Err(InstructionParseError::NoDirection),
     };
-    let steps: u32 = line[1..].parse().map_err(|_| Err(InstructionParseError::StepsParsingFailed))?;
-    Ok(Instruction {
-        direction,
-        steps
-    })
+    let steps = line[1..]
+        .parse()
+        .map_err(|_| InstructionParseError::StepsParsingFailed)?;
+    Ok(Instruction { direction, steps })
 }
 
 fn parse_input_path(arg: Option<String>) -> Result<Vec<Instruction>, InputPathError> {
     match arg.as_deref() {
         Some(path) => {
-            let content = fs::read_to_string(path.to_string()).map_err(|_| Err(InputPathError::UnableToReadFrom(path.to_string())))?;
+            let content = fs::read_to_string(path.to_string())
+                .map_err(|_| InputPathError::UnableToReadFrom(path.to_string()))?;
             content
                 .lines()
                 .map(|line| parse_instruction(line.trim()))
                 .collect::<Result<Vec<_>, _>>()
                 .map_err(InputPathError::ParseError)
-        },
-        None => Err(InputPathError::Missing)
+        }
+        None => Err(InputPathError::Missing),
     }
 }
 
-
-
-struct first_part;
-struct second_part;
-
-trait solution {
-    fn solve(input: Vec<String>);
-}
-
-impl solution for first_part {
-    fn solve(input: Vec<String>) {
-        let mut current_dial= DIAL_START;
-        let mut answer: u32 = 0;
-
-        for instruction in &input {
-            if let Some(first_char) = instruction.chars().next() {
-                let number_part = &instruction[1..];
-                let number: u32 = number_part.parse().unwrap();
-                match first_char {
-                    'L' => move_left(&mut current_dial, number, &mut answer),
-                    'R' => move_right(&mut current_dial, number, &mut answer),
-                    _ => unreachable!("input should have only L and R"),
-                }
-            }
-        }
-        println!("1st part answer is: {:?}", answer);
-    }
-}
-
-impl solution for second_part {
-    fn solve(input: Vec<String>) {
-        let mut current_dial= DIAL_START;
-        let mut answer: u32 = 0;
-
-        for instruction in &input {
-            if let Some(first_char) = instruction.chars().next() {
-                let number_part = &instruction[1..];
-                let number: u32 = number_part.parse().unwrap();
-                match first_char {
-                    'L' => move_left_second(&mut current_dial, number, &mut answer),
-                    'R' => move_right_second(&mut current_dial, number, &mut answer),
-                    _ => unreachable!("input should have only L and R"),
-                }
-            }
-        }
-        println!("2nd part answer is: {:?}", answer);
-    }
-}
-fn take_input() -> Vec<String> {
-    let file_path = env::args().nth(2);
-    let file_path = match file_path {
-        Some(file_path) => {
-            println!("reading file: {:?}", file_path);
-            file_path
-        },
-        None => {
-            panic!("no input path file given")
-        }
-    };
-    let file_content = fs::read_to_string(file_path).expect("issue while reading the input file");
-    let lines: Vec<String> = file_content
-        .lines()
-        .map(|line| line.trim().to_string())
-        .collect();
-    lines
-}
 fn move_right(current_dial: &mut u32, instruction: u32, answer: &mut u32) {
     *current_dial += instruction % 100;
     *current_dial %= 100;
@@ -152,7 +96,7 @@ fn move_right_second(current_dial: &mut u32, instruction: u32, answer: &mut u32)
     let temp_current = *current_dial;
     *current_dial += instruction % 100;
     *current_dial %= 100;
-    *answer += (instruction / 100);
+    *answer += instruction / 100;
     if temp_current > *current_dial {
         *answer += 1;
     }
@@ -168,13 +112,42 @@ fn move_left_second(current_dial: &mut u32, instruction: u32, answer: &mut u32) 
 }
 const DIAL_START: u32 = 50;
 
+fn solve_part_one(instructions: &Vec<Instruction>) -> u32 {
+    let mut current_dial = DIAL_START;
+    let mut answer = 0;
 
-fn main() {
-    let part = env::args().nth(1);
-    let input = take_input();
-    match part.as_deref() {
-        Some("1") => first_part::solve(input),
-        Some("2") => second_part::solve(input),
-        _ => eprintln!("usage: cargo run -- <1|2>"),
-    }
+    instructions
+        .iter()
+        .for_each(|instruction| match instruction.direction {
+            Direction::Left => move_left(&mut current_dial, instruction.steps, &mut answer),
+            Direction::Right => move_right(&mut current_dial, instruction.steps, &mut answer),
+        });
+    answer
+}
+
+fn solve_part_two(instructions: &Vec<Instruction>) -> u32 {
+    let mut current_dial = DIAL_START;
+    let mut answer = 0;
+
+    instructions
+        .iter()
+        .for_each(|instruction| match instruction.direction {
+            Direction::Left => move_left_second(&mut current_dial, instruction.steps, &mut answer),
+            Direction::Right => {
+                move_right_second(&mut current_dial, instruction.steps, &mut answer)
+            }
+        });
+    answer
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let inputs: Vec<String> = env::args().collect();
+    let part = parse_part(inputs.get(1).cloned())?;
+    let instructions = parse_input_path(inputs.get(2).cloned())?;
+    let answer = match part {
+        Part::First => solve_part_one(&instructions),
+        Part::Second => solve_part_two(&instructions),
+    };
+    println!("answer is: {:?}", answer);
+    Ok(())
 }
